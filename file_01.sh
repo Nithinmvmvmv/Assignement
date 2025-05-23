80edb46c0892df4a7cda46e88a4ab4491b7d3ed4
Nithinmv <nithin8964@gmail.com>
2025-05-23 14:37:07 +0530

    Assignement commited

--- file_01.sh
+++ file_01.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Hi,Welcome"
+
+#!/bin/bash
+
+# Check if a file is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 sample.txt"
+    exit 1
+fi
+
+FILE=$1
+
+# Process the file: extract words, sort them, count occurrences, sort by frequency, and display the top 5
+cat "$FILE" | tr -cs 'A-Za-z' '\n' | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | head -5
